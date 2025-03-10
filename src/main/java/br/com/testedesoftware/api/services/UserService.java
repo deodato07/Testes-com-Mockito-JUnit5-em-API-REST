aff671d60ec9785f4f9974c1d2ed2d1dddaa5a86
deodato07 <deodatomoreira07@gmail.com>
2025-03-10 12:38:11 -0300

    endpoint create pa User

--- src/main/java/br/com/testedesoftware/api/services/UserService.java
+++ src/main/java/br/com/testedesoftware/api/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.testedesoftware.api.services;
 
 import br.com.testedesoftware.api.domain.User;
+import br.com.testedesoftware.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
